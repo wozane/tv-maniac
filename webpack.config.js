36559bea46f6eb34dbc123a20a1946b9ac5d1609
Aneta Wozniak <wozniakaneta5@gmail.com>
2017-09-25 15:32:56 +0200

    Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,16 @@
+const HtmlWebpackPlugin = require ('html-webpack-plugin')
+
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: './dist/bundle.js',
+    filename: './dist/bundle-[hash].js',
   },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'Tv Maniac',
+  })],
   devtool: 'source-map',
+  devServer: {
+    contentBase:'./src',
+    // noInfo: true,
+  },
 }
