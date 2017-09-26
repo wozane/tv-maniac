5e83344f31944b06afd37df81f40029db9685657
Aneta Wozniak <wozniakaneta5@gmail.com>
2017-09-26 12:58:37 +0200

    Add Header and style for it

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Header from './Header'
 
 const App = props => (<div title="My Div">
+  <Header />
   <h1>{props.message}</h1>
 </div>)
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => (<nav className="navbar is-dark has-shadow">
+  <div className="navbar-menu container">
+    <div className="navbar-start" >
+      <a className="navbar-item is-tab">Home</a>
+      <a className="navbar-item is-tab">Search</a>
+      <a className="navbar-item is-tab">Contact</a>
+    </div>
+  </div>
+</nav>)
+
+export default Header
