a9fb67cdefc4c4c3453bb0199c356ed52120512b
Aneta Wozniak <wozniakaneta5@gmail.com>
2017-09-27 10:18:17 +0200

    Add axios to <SearchPage />

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -1,8 +1,18 @@
+import axios from 'axios'
+
 import React from 'react'
 import Search from './Search'
 
-const SearchPage = () => (<div>
-  <Search onSubmit={e => console.log(e)} />
-</div>)
+class SearchPage extends React.Component {
+  handleSearch = (query) => {
+    const apiUrl = `http://api.tvmaze.com/search/shows?q=${query}`
+    axios.get(apiUrl).then(console.log)
+  }
 
+  render() {
+    return (<div>
+      <Search onSubmit={this.handleSearch} />
+    </div>)
+  }
+}
 export default SearchPage
